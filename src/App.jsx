940995f4dda44a651fd851ebc5a9807c8ab8fbed
jinukrishnan <jinukrishnan.p@gmail.com>
2024-04-26 10:42:06 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import NavBar from "./componentss/navbar";
 import Home from "./pages/home";
+import { useState } from "react";
+
 import Detail from "./pages/Detail";
 import Cart from "./pages/Cart";
 
+
 export default function App() {
+  const [search, setSearch] = useState("");
   return (
     <main>
       <BrowserRouter>
-        <NavBar />
+        <NavBar setSearch={setSearch} />
         <Routes>
-          <Route path="/" Component={Home} />
+          <Route path="/" element={<Home search={search} />} />
           <Route path="/detail/:id" Component={Detail} />
           <Route path="/cart" Component={Cart} />
         </Routes>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,47 +1,106 @@
 import { memo, useEffect, useState } from "react";
-// import useFetch from "../custom_hooks/useFetch";
 import axios from "axios";
-import '../index.css'
 import { Link } from "react-router-dom";
-function Home() {
-    const [data,setData]=useState([]);
-    async function getData(){
-        const res=await axios.get('https://dummyjson.com/products');
-        setData([...res.data.products]);
-    }
-    useEffect(()=>{
-        getData();
+import "./style.css";
+import Loading from "../componentss/loading";
 
-    },[])
-   console.log(data);
-    return (
-        <main>
-            <div className="parent">
-                    {
-                        data.map((dt)=><Link to={`/detail/${dt.id}`} className="link" key={dt.id}>
-                        <div className="card" >
-                           
-                           <div className="image">
-                           <img src={dt.thumbnail} alt="" className="thumbnail" />
-                           </div>
-                           <div className="data">
-                            {
-                                dt.title.length<15?<h4>{dt.title}</h4>:<h4>{dt.title.substring(0, 15)}...</h4>
-                            }
-                            <h5>{dt.brand}</h5>
-                           <div className="number">
-                           <h4>$ {dt.price}</h4>
-                            <h6>rating : {dt.rating}</h6>
-                           </div>
+function Home({ search }) {
 
-                           </div>
-                          
-                        </div>
-                        </Link>)
-                    }
-            </div>
-        </main>
-    );
+  async function getData() {
+    const catRes = await axios.get("https://dummyjson.com/products/categories");
+    const res = await axios.get(`https://dummyjson.com/products?limit=100`);
+    setData([...res.data.products]);
+    setCategories([...catRes.data]);
+  }
+  const [filter, setFilter] = useState("");
+  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getData();
+  }, [])
+  if(data.length === 0) return <Loading />
+  return (
+    <main>
+      <div className="categories">
+        <section className="category" onClick={() => setFilter("")}>
+          all
+        </section>
+        {categories.map((item, index) => (
+          <section key={index} className="category" onClick={() => setFilter(item)}>
+            {item}
+          </section>
+        ))}
+      </div>
+      <div className="parent">
+        {data
+          .filter(i => i.title.toLowerCase().includes(search.toLowerCase()))
+          .filter(i => i.category.includes(filter))
+          .map((dt) => (
+            <Link to={`/detail/${dt.id}`} className="link" key={dt.id}>
+              <div className="card" key={dt.id}>
+                <div className="image">
+                  <img src={dt.thumbnail} alt="" className="thumbnail" loading="lazy" />
+                </div>
+                <div className="data">
+                  {
+                    dt.title.length < 15 ? <h4>{dt.title}</h4> : <h4>{dt.title.substring(0, 15)}...</h4>
+                  }
+                  <h5>{dt.brand}</h5>
+                  <div className="number">
+                    <h4>$ {dt.price}</h4>
+                    <h6>rating : {dt.rating}</h6>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+    </main>
+  );
 }
 
-export default memo(Home);
+// import '../index.css'
+// import { Link } from "react-router-dom";
+// function Home() {
+//   const [data, setData] = useState([]);
+//   async function getData() {
+//     const res = await axios.get('https://dummyjson.com/products');
+//     setData([...res.data.products]);
+//   }
+//   useEffect(() => {
+//     getData();
+
+//   }, [])
+//   console.log(data);
+//   return (
+//     <main>
+//       <div className="parent">
+//         {
+//           data.map((dt) => <Link to={`/detail/${dt.id}`} className="link" key={dt.id}>
+//             <div className="card" >
+
+//               <div className="image">
+//                 <img src={dt.thumbnail} alt="" className="thumbnail" />
+//               </div>
+//               <div className="data">
+//                 {
+//                   dt.title.length < 15 ? <h4>{dt.title}</h4> : <h4>{dt.title.substring(0, 15)}...</h4>
+//                 }
+//                 <h5>{dt.brand}</h5>
+//                 <div className="number">
+//                   <h4>$ {dt.price}</h4>
+//                   <h6>rating : {dt.rating}</h6>
+//                 </div>
+
+//               </div>
+
+//             </div>
+//           </Link>)
+//         }
+//       </div>
+//     </main>
+//   );
+// }
+
+export default memo(Home); 
